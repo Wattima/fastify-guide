1104cab9357b7cb8e5452f00a1af772c429a3d75
Wattima <darwattima@gmail.com>
2023-09-06 11:49:14 +0300

    make first hello world get request

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const app = fastify({logger:true})
 
 const PORT = process.env.PORT || 8000
 
+app.get('/hello', (request, reply) => {
+    return 'Hello World'
+ })
 
 app.listen(PORT).catch((error) => {
     app.log.error(error)
