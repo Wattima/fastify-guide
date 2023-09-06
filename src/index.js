2e0a65b92b6cc32cd3617edec15ea4198e4d734d
Wattima <darwattima@gmail.com>
2023-09-06 15:47:06 +0300

    Create query request for gender

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ const app = fastify({logger:true})
 const PORT = process.env.PORT || 8000
 
 app.get('/getUsers', (request, reply) => {
-    return users;
+    const {gender} = request.query
+
+    if (!gender) return users
+
+    const filteredUsers = users.filter((user) => user.gender.toLowerCase() == gender.toLowerCase())
+    return filteredUsers;
  })
+
+ // domain.com/getUsers?gender=male
 
 app.listen(PORT).catch((error) => {
     app.log.error(error)
